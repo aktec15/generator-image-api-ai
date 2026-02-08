e5b6e5147fcd0bd91f0923a670b2c879f70a5365
aktec <aktec@mail.ru>
2026-02-08 15:10:29 +0300

    generator image api AI

---
+++ main.py
@@ -0,0 +1,28 @@
+import requests
+import base64
+import config
+
+
+url = 'https://api.reve.com/v1/image/create'
+
+headers = {
+    'Authorization' : f'Bearer {config.api_key}',
+    'Accept' : 'application/json',
+    'Content-Type' : 'application/json'
+}
+
+data = {
+    "prompt": "cat",
+    "aspect_ratio": "16:9",
+    "version": "latest"
+}
+
+result = requests.post(url=url, headers=headers, data=data)
+image = result.json()['image']
+
+print(result.json())
+
+
+with open("output.png", "wb") as fh:
+    fh.write(base64.decode(image))
+
